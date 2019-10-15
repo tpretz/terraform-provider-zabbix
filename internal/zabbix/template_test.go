20e8b45fb23c1341c557269063b5ca3d37279650
gaetan <gaetan.depin@fr.clara.net>
2019-10-15 15:51:02 +0200

    edit template test

--- internal/zabbix/template_test.go
+++ internal/zabbix/template_test.go
@@ -6,8 +6,30 @@ import (
 	dd "github.com/claranet/go-zabbix-api"
 )
 
+func CreateTemplate(hostGroup *dd.HostGroup, t *testing.T) *dd.Template {
+	template := dd.Templates{dd.Template{
+		Host:   "template name",
+		Groups: dd.HostGroups{*hostGroup},
+	}}
+	err := getAPI(t).TemplatesCreate(template)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return &template[0]
+}
+
+func DeleteTemplate(template *dd.Template, t *testing.T) {
+	err := getAPI(t).TemplatesDelete(dd.Templates{*template})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
 func TestTemplates(t *testing.T) {
 	api := getAPI(t)
+
+	hostGroup := CreateHostGroup(t)
+	defer DeleteHostGroup(hostGroup, t)
 
 	templates, err := api.TemplatesGet(dd.Params{})
 	if err != nil {
@@ -17,4 +39,22 @@ func TestTemplates(t *testing.T) {
 	if len(templates) == 0 {
 		t.Fatal("No templates were obtained")
 	}
+
+	template := CreateTemplate(hostGroup, t)
+	if template.TemplateID == "" {
+		t.Errorf("Template id is empty %#v", template)
+	}
+
+	_, err = api.TemplateGetByID(template.TemplateID)
+	if err != nil {
+		t.Error(err)
+	}
+
+	template.Name = "new template name"
+	err = api.TemplatesUpdate(dd.Templates{*template})
+	if err != nil {
+		t.Error(err)
+	}
+
+	DeleteTemplate(template, t)
 }
