a581ebe2f1eac36ca372293a49cd13d6438dad35
Thomas Pressnell <tom@pressnell.uk>
2020-02-29 15:49:25 +0000

    starting on schema

--- provider/resource_item.go
+++ provider/resource_item.go
@@ -1,37 +1,75 @@
 package provider
 
 import (
-        "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
 func resourceItem() *schema.Resource {
-        return &schema.Resource{
-                Create: resourceItemCreate,
-                Read:   resourceItemRead,
-                Update: resourceItemUpdate,
-                Delete: resourceItemDelete,
+	return &schema.Resource{
+		Create: resourceItemCreate,
+		Read:   resourceItemRead,
+		Update: resourceItemUpdate,
+		Delete: resourceItemDelete,
 
-                Schema: map[string]*schema.Schema{
-                        "address": &schema.Schema{
-                                Type:     schema.TypeString,
-                                Required: true,
-                        },
-                },
-        }
+		Schema: map[string]*schema.Schema{
+			"itemid": &schema.Schema{
+				Type:        schema.TypeString,
+				Computed:    true,
+				ForceNew:    true,
+				Description: "Zabbix ID",
+			},
+			"delay": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Item Delay",
+			},
+			"hostid": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Host ID",
+			},
+			"interfaceid": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Host ID",
+			},
+			"key": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"type": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"url": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: false,
+				Optional: true,
+			},
+			"value_type": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
 }
 
 func resourceItemCreate(d *schema.ResourceData, m interface{}) error {
-        return resourceItemRead(d, m)
+	return resourceItemRead(d, m)
 }
 
 func resourceItemRead(d *schema.ResourceData, m interface{}) error {
-        return nil
+	return nil
 }
 
 func resourceItemUpdate(d *schema.ResourceData, m interface{}) error {
-        return resourceItemRead(d, m)
+	return resourceItemRead(d, m)
 }
 
 func resourceItemDelete(d *schema.ResourceData, m interface{}) error {
-        return nil
+	return nil
 }
