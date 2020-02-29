c0d0d13d9d3af6eba1f50c1d52a3d49b07080d95
Thomas Pressnell <thomas.pressnell@sky.uk>
2020-02-29 13:19:02 +0000

    initial skel

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tpretz/terraform-provider-zabbix
+
+require github.com/hashicorp/terraform-plugin-sdk v1.7.0
---
+++ provider/provider.go
@@ -0,0 +1,11 @@
+package provider
+
+import (
+        "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func Provider() *schema.Provider {
+        return &schema.Provider{
+                ResourcesMap: map[string]*schema.Resource{},
+        }
+}
