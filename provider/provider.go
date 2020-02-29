cf7efcf1da4e986f4572c2475ee1ca7157df74ae
Thomas Pressnell <tom@pressnell.uk>
2020-02-29 15:17:18 +0000

    work

--- provider/provider.go
+++ provider/provider.go
@@ -1,21 +1,43 @@
 package provider
 
 import (
-        "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/tpretz/go-zabbix-api"
+	"log"
 )
 
 func Provider() *schema.Provider {
-        return &schema.Provider{
-                ResourcesMap: map[string]*schema.Resource{},
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"username": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.MultiEnvDefaultFunc([]string{"ZABBIX_USER", "ZABBIX_USERNAME"}, nil),
+			},
+			"password": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.MultiEnvDefaultFunc([]string{"ZABBIX_PASS", "ZABBIX_PASSWORD"}, nil),
+			},
+			"url": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.MultiEnvDefaultFunc([]string{"ZABBIX_URL", "ZABBIX_SERVER_URL"}, nil),
+			},
+		},
+		ResourcesMap: map[string]*schema.Resource{
+			"zabbix_item": resourceItem(),
+		},
 		ConfigureFunc: providerConfigure,
 	}
 }
 
 func providerConfigure(d *schema.ResourceData) (meta interface{}, err error) {
+	log.Printf("[TRACE] Started zabbix provider init")
 	api := zabbix.NewAPI(d.Get("url").(string))
-	_, err = api.Login(d.Get("user").(string), d.Get("password").(string))
-        meta = api
+	_, err = api.Login(d.Get("username").(string), d.Get("password").(string))
+	meta = api
+	log.Printf("[TRACE] Started zabbix provider got error: %+v", err)
 
 	return
 }
