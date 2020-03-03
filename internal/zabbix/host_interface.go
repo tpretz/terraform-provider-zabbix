15c465005afcd73fe29d7e357d873a148881e55b
Thomas Pressnell <thomas.pressnell@sky.uk>
2020-03-03 20:44:06 +0000

    adding interfaceid

--- internal/zabbix/host_interface.go
+++ internal/zabbix/host_interface.go
@@ -22,12 +22,13 @@ const (
 // HostInterface represents zabbix host interface type
 // https://www.zabbix.com/documentation/3.2/manual/api/reference/hostinterface/object
 type HostInterface struct {
-	DNS   string        `json:"dns"`
-	IP    string        `json:"ip"`
-	Main  string        `json:"main"`
-	Port  string        `json:"port"`
-	Type  InterfaceType `json:"type"`
-	UseIP string        `json:"useip"`
+	InterfaceID string        `json:"interfaceid,omitempty"`
+	DNS         string        `json:"dns"`
+	IP          string        `json:"ip"`
+	Main        string        `json:"main"`
+	Port        string        `json:"port"`
+	Type        InterfaceType `json:"type"`
+	UseIP       string        `json:"useip"`
 }
 
 // HostInterfaces is an array of HostInterface
