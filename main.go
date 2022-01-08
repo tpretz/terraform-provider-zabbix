3a5cbbb1deefddf1c5ba779e65b1115fac4cc48c
Thomas Pressnell <tom@pressnell.uk>
2022-01-08 16:29:58 +0000

    minor change sdkv2

--- main.go
+++ main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/tpretz/terraform-provider-zabbix/provider"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() terraform.ResourceProvider {
-			return provider.Provider()
-		},
+		ProviderFunc: provider.Provider,
 	})
 }
