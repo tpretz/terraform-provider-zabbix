01bb74709eae13d1661b4024ca2cce1cbafe96f0
Thomas Pressnell <tom@pressnell.uk>
2022-02-16 22:27:16 +0000

    adding application test

--- Makefile
+++ Makefile
@@ -1,9 +1,26 @@
 
+export TF_ACC=1
+export TF_ACC_LOG_PATH=acc.log
+export TF_ACC_STATE_LINEAGE=1
+export ZABBIX_USER=Admin
+export ZABBIX_PASS=zabbix
 
-testacc:
-	TF_ACC=1 ZABBIX_URL=http://zabbix-web-40:8080/api_jsonrpc.php ZABBIX_USER=Admin ZABBIX_PASS=zabbix go test ./provider
-	TF_ACC=1 ZABBIX_URL=http://zabbix-web-50:8080/api_jsonrpc.php ZABBIX_USER=Admin ZABBIX_PASS=zabbix go test ./provider
-	TF_ACC=1 ZABBIX_URL=http://zabbix-web-54:8080/api_jsonrpc.php ZABBIX_USER=Admin ZABBIX_PASS=zabbix go test ./provider
+.PHONY: testacc
+testacc: cleanlog test40 test50 test54
 
-testquick:
-	TF_ACC=1 ZABBIX_URL=http://zabbix-web-54:8080/api_jsonrpc.php ZABBIX_USER=Admin ZABBIX_PASS=zabbix go test ./provider
+.PHONY: cleanlog
+cleanlog:
+	rm provider/acc.log || true
+
+.PHONY: test40
+test40:
+	ZABBIX_URL=http://zabbix-web-40:8080/api_jsonrpc.php go test ./provider
+
+.PHONY: test50
+test50:
+	ZABBIX_URL=http://zabbix-web-50:8080/api_jsonrpc.php go test ./provider
+
+.PHONY: test54
+test54:
+	ZABBIX_URL=http://zabbix-web-54:8080/api_jsonrpc.php go test ./provider
+
--- provider/resource_application_test.go
+++ provider/resource_application_test.go
@@ -1,1 +1,71 @@
 package provider
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/tpretz/go-zabbix-api"
+)
+
+func TestAccResourceApplication(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{ // lazy load config, needed for skipfunc that look at meta
+				Config: `
+resource "zabbix_hostgroup" "lazyconfigload" {
+	name = "lazyload" 
+}
+`,
+			},
+			{ // simple create
+				SkipFunc: func() (bool, error) {
+					api := testAccProvider.Meta().(*zabbix.API)
+					return api.Config.Version >= 50400, nil
+				},
+				Config: `
+resource "zabbix_hostgroup" "testgrp" {
+	name = "test-group" 
+}
+resource "zabbix_template" "testtmpl" {
+	groups = [ zabbix_hostgroup.testgrp.id ]
+	host = "test-template"
+}
+resource "zabbix_application" "testapp" {
+	name = "test-app" 
+	hostid = zabbix_template.testtmpl.id
+}
+`,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("zabbix_application.testapp", "name", "test-app"),
+				),
+			},
+			{ // rename
+				SkipFunc: func() (bool, error) {
+					api := testAccProvider.Meta().(*zabbix.API)
+					return api.Config.Version >= 50400, nil
+				},
+				Config: `
+resource "zabbix_hostgroup" "testgrp" {
+	name = "test-group" 
+}
+resource "zabbix_template" "testtmpl" {
+	groups = [ zabbix_hostgroup.testgrp.id ]
+	host = "test-template"
+}
+resource "zabbix_application" "testapp" {
+	name = "test-app-renamed" 
+	hostid = zabbix_template.testtmpl.id
+}
+`,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("zabbix_application.testapp", "name", "test-app-renamed"),
+				),
+			},
+			// TODO ref to an item
+		},
+	})
+}
